3e5eb77e7b5e3538289c850b7a13264abd675492
Danielle Nunes <141048448+Danielle-sn@users.noreply.github.com>
2024-07-22 15:13:14 -0300

    pausar o jogo

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -1,13 +1,19 @@
 package main;
 
+import Telas.Tela;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler  implements KeyListener{
-
+        Tela tj;
         public boolean upPressed, downPressed, leftPressed,rightPressed;
         //DEBUG
         public boolean  checkDrawTime = false;
+
+        public KeyHandler(Tela tj){
+            this.tj = tj;
+        }
 
         @Override
         public void keyTyped(KeyEvent e) {
@@ -18,55 +24,62 @@ public class KeyHandler  implements KeyListener{
         public void keyPressed(KeyEvent e) {
 
             int code = e.getKeyCode(); //retorna uma int da tecla pressionada
-            if(code == KeyEvent.VK_W || code == KeyEvent.VK_UP ){
+            if (code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {
                 upPressed = true;
             }
 
-            if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN ){
+            if (code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
                 downPressed = true;
             }
 
-            if(code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT ){
+            if (code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
                 leftPressed = true;
             }
 
-            if(code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT ){
+            if (code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
                 rightPressed = true;
             }
-            //DEBUG
-            if(code == KeyEvent.VK_T){
-                if(checkDrawTime == false){
-                    checkDrawTime = true;
-                } else if(checkDrawTime == true){
-                    checkDrawTime = false;
+            if(code == KeyEvent.VK_P){
+                if(tj.gameState == tj.playState){
+                    tj.gameState = tj.pauseState;
+                } else if (tj.gameState == tj.pauseState){
+                    tj.gameState = tj.playState;
                 }
             }
+                //DEBUG
+//            if (code == KeyEvent.VK_T) {
+//                if (checkDrawTime == false) {
+//                    checkDrawTime = true;
+//                } else if (checkDrawTime == true) {
+//                    checkDrawTime = false;
+//                }
+//            }
 
         }
 
 
-        @Override
-        public void keyReleased(KeyEvent e) {
+            @Override
+            public void keyReleased (KeyEvent e){
 
-            int code = e.getKeyCode();
+                int code = e.getKeyCode();
 
-            if(code == KeyEvent.VK_W || code == KeyEvent.VK_UP ){
-                upPressed = false;
-            }
+                if (code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {
+                    upPressed = false;
+                }
 
-            if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN ){
-                downPressed = false;
-            }
+                if (code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
+                    downPressed = false;
+                }
 
-            if(code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT ){
-                leftPressed = false;
-            }
+                if (code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
+                    leftPressed = false;
+                }
 
-            if(code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT ) {
-                rightPressed = false;
+                if (code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
+                    rightPressed = false;
+                }
+
             }
 
         }
-
-}
 
